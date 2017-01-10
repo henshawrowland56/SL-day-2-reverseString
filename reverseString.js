195523873fc11d2eb40a3385a41059a1af2001c8
henshaw rowland <henshawrowland@yahoo.com>
2017-01-10 05:24:54 +0100

    Added completely tested reversString.js file

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+module.exports = {
+	 reverseString: function(str) {
+  		var reversedString = "";
+  		if(str.length < 1){return null;}
+  		for(var i = str.length - 1; i>=0; i--){
+    		reversedString += str[i];
+  		}
+  		if(reversedString === str){return true;}
+  		return reversedString;
+	}
+}
